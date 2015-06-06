08aed1459bcf46cb7c272d326a9368af4044e7eb
Matt Pope <matt@parallelpublicworks.com>
2015-06-06 13:27:56 -0700

    Removed keys from sample

--- fatsecret_example.py
+++ fatsecret_example.py
@@ -6,8 +6,8 @@ from pandas import *
 
 #-----------------------------------
 #To get this working you'll need to add a config.py file with these two lines:
-#consumer_key='10ec942b913647f88743f6451c088b2b'
-#consumer_secret='eb5d8bb2062a4d7eb3c78d40d9b8968e'
+#consumer_key='YOURKEYHERE'
+#consumer_secret='YOURKEYHERE'
 #get your keys here: http://platform.fatsecret.com/api/ 
 
 fs=Fatsecret(consumer_key,consumer_secret)
